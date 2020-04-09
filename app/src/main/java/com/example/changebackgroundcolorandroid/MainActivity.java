f99c67fcd00b571e041646dfd250fd5b9c4388e8
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-04-09 23:27:46 +0300

    Set random color screen.

---
+++ app/src/main/java/com/example/changebackgroundcolorandroid/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.changebackgroundcolorandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        Random random = new Random();
+
+        int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+
+        layout.setBackgroundColor(color);
+
+        return super.onTouchEvent(event);
+    }
+}
